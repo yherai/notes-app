9455813e3b63b106b37c33349a10c10005bf0811
Jorge Garcia <jagu.ittis@gmail.com>
2020-02-19 18:19:56 +0100

    Include yargs npm library to manage arguments of our application
    Create some commands to add, remove, read and list notes

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const command = process.argv[2];
+// Customize yargs version
+yargs.version('1.1.0');
 
-if (command === 'add') {
-  console.log('Adding note!');
-} else if (command === 'remove') {
-  console.log('Removing note!');
-}
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function() {
+    console.log('Adding a new note!');
+  }
+});
 
-console.log(process.argv);
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function() {
+    console.log('Removing a note!');
+  }
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function() {
+    console.log('Reading a note!');
+  }
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: function() {
+    console.log('Listing all notes!');
+  }
+});
+
+console.log(yargs.argv);
