b684dabfda1e67c9d245ef03cdee39435eb83769
Jorge Garcia <jagu.ittis@gmail.com>
2021-05-10 19:25:47 +0200

    Include removeNote functionality
    Improve log messages with chalk

--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const chalk = require('chalk');
 
 const getNotes = () => {
   return 'Your notes...';
@@ -12,8 +13,22 @@ const addNote = (title, body) => {
   if (duplicateNotes.length === 0) {
     notes.push({ title, body });
     saveNotes(notes);
+    console.log(chalk.green.inverse('New note added'));
   } else {
-    console.log('Note title taken!');
+    console.log(chalk.red.inverse('Note title taken!'));
+  }
+};
+
+const removeNote = (title) => {
+  const notes = loadNotes();
+
+  const notesToKeep = notes.filter((note) => note.title !== title);
+
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse('Note removed'));
+    saveNotes(notesToKeep);
+  } else {
+    console.log(chalk.red.inverse('No note found'));
   }
 };
 
@@ -32,4 +47,4 @@ const loadNotes = () => {
   }
 };
 
-module.exports = { getNotes, addNote };
+module.exports = { getNotes, addNote, removeNote };
